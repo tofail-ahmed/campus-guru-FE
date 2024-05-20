36ea7177506d7f3f236217e591882bdfbe31003a
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-05-20 18:22:20 +0600

    dark/light mode added with redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import { toggleDarkMode } from "@/redux/theme/themeSlice";
 import Image from "next/image";
 
 export default function Home() {
-
+  const darkMode=useAppSelector((state)=>state.theme.darkMode);
+  const dispatch=useAppDispatch();
+  const handleDarkMode=()=>{
+    dispatch(toggleDarkMode())
+  }
   
   return (
    <>
    <h1 className="text-4xl font-bold flex items-center justify-center">Hello Campus Guru</h1>
-   {/* <button onClick={handleDarkMode}>Change Theme</button> */}
+   <button onClick={handleDarkMode}>Change Theme</button>
    </>
   );
 }
